b9ea9bac189be79f38f1eb374a74437614b0fdcf
abisuka <a_654_a@hotmail.com>
2019-01-09 13:42:06 +0700

    add sales.java -close#3

---
+++ sales.java
@@ -0,0 +1,21 @@
+package th.sut.sa.demo;
+
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+
+public class DemoApplication {
+	
+	private int id
+;
+public static void main(String[] args) {
+
+
+
+	}
+
+}
